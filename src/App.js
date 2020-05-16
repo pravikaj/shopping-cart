37a76624f58428399b7d8198f76c2790261eef80
pravikaj <pravika.joshi@gmail.com>
2020-05-15 21:10:55 -0500

    Added product grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import { Grid, makeStyles, Box } from "@material-ui/core";
+
+const sizes = { S: "S", M: "M", L: "L", XL: "XL" };
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  const productids = Object.keys(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch("./data/products.json");
@@ -13,11 +19,28 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
+    <React.Fragment>
+      <Grid container justify="center" spacing="5">
+        {products.map((product, index) => (
+          <Grid key={product.sku} item>
+            <ProductCard
+              product={product}
+              productid={productids[index]}
+              className={product.title}
+            ></ProductCard>
+          </Grid>
+        ))}
+      </Grid>
       {products.map((product) => (
-        <li key={product.sku}>{product.title}</li>
+        <ProductCard product={product} productid={product.sku} />
       ))}
-    </ul>
+
+      <ul>
+        {products.map((product) => (
+          <li key={product.sku}>{product.title}</li>
+        ))}
+      </ul>
+    </React.Fragment>
   );
 };
 
---
+++ src/ProductCard.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
+
+const useStyles = makeStyles({
+  root: {
+    // minWidth: 275,
+    width: 300,
+    height: 630,
+    textAlign: "center",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const sizes = { S: "S", M: "M", L: "L", XL: "XL" };
+
+const ProductCard = ({ product, productid }) => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("./data/products.json");
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          component="img"
+          src={"data/products/" + productid + "_1.jpg"}
+          title={product.title}
+        />
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {product.title}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {product.description}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {product.currencyFormat}
+            {product.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small">S</Button>
+        <Button size="small">M</Button>
+        <Button size="small">L</Button>
+        <Button size="small">XL</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProductCard;
